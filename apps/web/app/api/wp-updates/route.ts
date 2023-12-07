20f057057182011cebbaa1c0fcb0daccfcbcb04e
shipmaal <100368040+shipmaal@users.noreply.github.com>
2023-12-07 17:12:04 -0500

    Add dynamic export in wp-updates route

--- apps/web/app/api/wp-updates/route.ts
+++ apps/web/app/api/wp-updates/route.ts
@@ -1,5 +1,6 @@
 import { revalidateTag } from 'next/cache'
 
+export const dynamic = 'force-dynamic'
 export async function GET() {
     const res = await fetch('https://data.mongodb-api.com/...')
     const data = await res.json()
