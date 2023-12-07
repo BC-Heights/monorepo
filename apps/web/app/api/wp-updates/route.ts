9f6e9773046d237d56852c2f9799cbe1b104f22c
shipmaal <100368040+shipmaal@users.noreply.github.com>
2023-12-07 17:15:32 -0500

    Add POST route for webhook

--- apps/web/app/api/wp-updates/route.ts
+++ apps/web/app/api/wp-updates/route.ts
@@ -1,3 +1,4 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import { revalidateTag } from 'next/cache'
 
 export const dynamic = 'force-dynamic'
@@ -7,3 +8,14 @@ export async function GET() {
     revalidateTag('posts')
     return Response.json(data)
 }
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+    // This function will be triggered when your webhook sends a POST request
+    // You can access the data sent by the webhook in req.body
+  
+    // Trigger a revalidation of your data here
+    revalidateTag('posts')
+    console.log('Webhook received')
+  
+    res.status(200).json({ message: 'Webhook received' })
+  }
