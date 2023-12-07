b21ce5a8afa18af3225d0a801479529274e67cf9
shipmaal <100368040+shipmaal@users.noreply.github.com>
2023-12-07 16:14:56 -0500

    testing for wp webhooks

---
+++ apps/web/app/api/wp-updates/route.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { revalidateTag } from 'next/cache'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    // Trigger a revalidation of your data here
+    revalidateTag('Posts')
+
+    res.status(200).json({ message: 'Webhook received' })
+  } else {
+    res.status(405).json({ message: 'Method not allowed' })
+  }
+}
