5a4697b49f88c6f04e429c12a1e9b85f832721ad
shipmaal <100368040+shipmaal@users.noreply.github.com>
2023-12-07 17:01:00 -0500

    same thing updating api route

--- apps/web/app/api/wp-updates/route.ts
+++ apps/web/app/api/wp-updates/route.ts
@@ -1,13 +1,8 @@
-import { NextApiRequest, NextApiResponse } from 'next'
 import { revalidateTag } from 'next/cache'
 
-export default function POST(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    // Trigger a revalidation of your data here
-    revalidateTag('Posts')
-
-    res.status(200).json({ message: 'Webhook received' })
-  } else {
-    res.status(405).json({ message: 'Method not allowed' })
-  }
+export async function GET() {
+    const res = await fetch('https://data.mongodb-api.com/...')
+    const data = await res.json()
+    revalidateTag('posts')
+    return Response.json(data)
 }
