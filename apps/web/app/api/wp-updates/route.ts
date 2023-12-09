6c3d938d941b100c7aab893762d01de5e2f32e5d
shipmaal <100368040+shipmaal@users.noreply.github.com>
2023-12-09 16:27:25 -0500

    added isr to posts linked to the main page

--- apps/web/app/api/wp-updates/route.ts
+++ apps/web/app/api/wp-updates/route.ts
@@ -3,7 +3,14 @@ import { revalidateTag, revalidatePath } from 'next/cache'
 
 
 export async function POST(req: NextRequest) {
-    revalidateTag('posts')
-    revalidatePath('/')
-    return NextResponse.json({ received: true, now:  Date.now() })
+  const data = await req.json();
+  const { post_permalink } = data;
+  const lastSlashIndex = post_permalink.lastIndexOf('/');
+  const secondToLastSlashIndex = post_permalink.lastIndexOf('/', lastSlashIndex - 1);
+  const newHref = post_permalink.substring(secondToLastSlashIndex === -1 ? lastSlashIndex + 1 : secondToLastSlashIndex + 1);
+
+  revalidateTag('posts');
+  revalidatePath('/');
+  revalidatePath(newHref);
+  return NextResponse.json({ received: true, now:  Date.now() });
   }
