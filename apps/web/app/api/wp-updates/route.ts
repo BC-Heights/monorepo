eb0c7ede60ecd1ac78ca481aa1e6e69fe817440a
shipmaal <100368040+shipmaal@users.noreply.github.com>
2024-01-05 17:12:36 -0600

    Update server and app build manifests

--- apps/web/app/api/wp-updates/route.ts
+++ apps/web/app/api/wp-updates/route.ts
@@ -1,16 +1,18 @@
+'use server'
+
 import { NextRequest, NextResponse } from 'next/server'
 import { revalidateTag, revalidatePath } from 'next/cache'
 
 
 export async function POST(req: NextRequest) {
-  const data = await req.json();
-  const { post_permalink } = data;
-  const lastSlashIndex = post_permalink.lastIndexOf('/');
-  const secondToLastSlashIndex = post_permalink.lastIndexOf('/', lastSlashIndex - 1);
-  const newHref = post_permalink.substring(secondToLastSlashIndex === -1 ? lastSlashIndex + 1 : secondToLastSlashIndex + 1);
+  // const data = await req.json();
+  // const { post_permalink } = data;
+  // const lastSlashIndex = post_permalink.lastIndexOf('/');
+  // const secondToLastSlashIndex = post_permalink.lastIndexOf('/', lastSlashIndex - 1);
+  // const newHref = post_permalink.substring(secondToLastSlashIndex === -1 ? lastSlashIndex + 1 : secondToLastSlashIndex + 1);
 
   revalidateTag('posts');
   revalidatePath('/');
-  revalidatePath(newHref);
+  // revalidatePath(newHref);
   return NextResponse.json({ received: true, now:  Date.now() });
   }
