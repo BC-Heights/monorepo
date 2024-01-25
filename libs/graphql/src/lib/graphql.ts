1aafffc03f85f57b4a75af89e3545c0b0790526c
shipmaal <100368040+shipmaal@users.noreply.github.com>
2024-01-25 17:58:34 -0500

    Add tag control to graphql functions

--- libs/graphql/src/lib/graphql.ts
+++ libs/graphql/src/lib/graphql.ts
@@ -6,49 +6,60 @@ import {
   GetImageUrlDocument, GetImageUrlQuery } from "./queries.generated";
 import { getClient } from "@the-heights/apollo-client";
 
-export const GetPostsByCat = unstable_cache(async (first, categoryName) => {
-  const { data: { posts } } = await getClient().query<GetPostsByCatQuery>({
-    query: GetPostsByCatDocument,
-    variables: { first: first, categoryName: categoryName },
-  });
-  return posts;
-  }, ['posts'], {
-    tags: ['posts'],
-  }
-);
 
-export const GetPostBySlug = unstable_cache(async (slug) => {
-  const { data: { postBy} } = await getClient().query<GetPostBySlugQuery>({
-    query: GetPostBySlugDocument,
-    variables: { slug: slug },
-  });
-  return postBy;
-  }, ['posts'],
-  {
-    tags: ['posts'],
-  }
-);
+export async function GetPostsByCat(first: number, categoryName: string, tags = ['posts']) {
+  const PostsByCat = unstable_cache(async (first, categoryName) => {
+    const { data: { posts } } = await getClient().query<GetPostsByCatQuery>({
+      query: GetPostsByCatDocument,
+      variables: { first: first, categoryName: categoryName },
+    });
+    return posts;
+    }, ['posts'], {
+      tags: tags,
+    });
+    return await PostsByCat(first, categoryName);
+}
 
-export const SearchPosts = unstable_cache(async (search, first, after) => {
-  const { data: { posts } } = await getClient().query<SearchPostsQuery>({
-    query: SearchPostsDocument,
-    variables: { search: search, first: first, after: after },
-  });
-  return posts;
-  }, ['posts'],
-  {
-    tags: ['posts'],
-  }
-);
 
-export const GetImageUrl = unstable_cache(async (id) => {
-  const { data: { mediaItemBy: sourceUrl } } = await getClient().query<GetImageUrlQuery>({
-    query: GetImageUrlDocument,
-    variables: { id: id },
-  });
-  return sourceUrl;
-  }, ['images'],
-  {
-    tags: ['images'],
-  }
-);
+export async function GetPostBySlug(slug: string, tags = ['posts']) {
+  const PostBySlug = unstable_cache(async (slug) => {
+    const { data: { postBy} } = await getClient().query<GetPostBySlugQuery>({
+      query: GetPostBySlugDocument,
+      variables: { slug: slug },
+    });
+    return postBy;
+    }, ['posts'],
+    {
+      tags: tags,
+    });
+    return await PostBySlug(slug);
+}
+
+
+export async function SearchPosts(search: string, first: number, after: string, tags = ['posts']) {
+  const Posts = unstable_cache(async (search, first, after) => {
+    const { data: { posts } } = await getClient().query<SearchPostsQuery>({
+      query: SearchPostsDocument,
+      variables: { search: search, first: first, after: after },
+    });
+    return posts;
+    }, ['posts'],
+    {
+      tags: tags,
+    });
+    return await Posts(search, first, after);
+}
+
+export async function GetImageUrl(id: string, tags = ['images']) {
+  const ImageUrl = unstable_cache(async (id) => {
+    const { data: { mediaItemBy: sourceUrl } } = await getClient().query<GetImageUrlQuery>({
+      query: GetImageUrlDocument,
+      variables: { id: id },
+    });
+    return sourceUrl;
+    }, ['images'],
+    {
+      tags: tags,
+    });
+  return await ImageUrl(id);
+}
