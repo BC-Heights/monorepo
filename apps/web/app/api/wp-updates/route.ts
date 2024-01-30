7f0c0fb6e51c990105d82ee07731db5428bc3ff7
shipmaal <100368040+shipmaal@users.noreply.github.com>
2024-01-30 10:45:44 -0500

    Refactor wp-updates route to export POST function

--- apps/web/app/api/wp-updates/route.ts
+++ apps/web/app/api/wp-updates/route.ts
@@ -1,11 +1,9 @@
-'use server';
-
 import { NextRequest, NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
 import PostData from './PostData';
 export const runtime = 'edge'
 
-async function POST(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const data = (await req.json()) as PostData;
   const apiKey = req.headers.get('x-api-key');
   if (apiKey !== process.env.WP_WEBHOOK) {
