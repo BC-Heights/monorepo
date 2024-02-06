f1215aea68bb5a4bef6bbd6bf34b87fca24778bd
shipmaal <100368040+shipmaal@users.noreply.github.com>
2024-02-06 17:46:34 -0500

    Refactor WP updates route and add API key validation

--- apps/web/app/api/wp-updates/route.ts
+++ apps/web/app/api/wp-updates/route.ts
@@ -1,35 +1,23 @@
 import { NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
 import PostData from './PostData';
-import { log, withAxiom, AxiomRequest } from 'next-axiom';
-export const runtime = 'edge';
+import { log, AxiomRequest } from 'next-axiom';
 
-
-export const GET = withAxiom(
-  (req: AxiomRequest) => async (req: AxiomRequest) => {
-    const data = await req.json();
-    const apiKey = req.headers.get('x-api-key');
-    if (apiKey !== process.env.WP_WEBHOOK) {
-      return NextResponse.error();
-    }
-
-    // const cats = data.taxonomies.category.map((cat) => cat.name.toLowerCase());
-    // console.log('POST', cats);
-    console.log({
-      ...data,
-      post: { ...data.post, post_content: undefined },
-      post_before: undefined,
-    });
-    log.info('POST', {
-      ...data,
-      post: { ...data.post, post_content: undefined },
-      post_before: undefined,
-    });
-    // console.log(data.taxonomies.category.map(cat))
-
-    // cats.forEach((cat) => revalidateTag(cat));
-    // to do revalidte indivudal sub section pages
-
-    return NextResponse.json({ received: true, now: Date.now(), data: data });
+export async function POST(req: AxiomRequest) {
+  const data = (await req.json());
+  const apiKey = req.headers.get('x-api-key');
+  if (apiKey !== process.env.WP_WEBHOOK) {
+    return NextResponse.error();
   }
-);
+
+  // const cats = data.taxonomies.category.map((cat) => cat.name.toLowerCase());
+  // console.log('POST', cats);
+  console.log({ ...data, post: { ...data.post, post_content: undefined }, post_before: undefined });
+  log.debug('POST', { ...data, post: { ...data.post, post_content: undefined }, post_before: undefined });
+  // console.log(data.taxonomies.category.map(cat))
+
+  // cats.forEach((cat) => revalidateTag(cat));
+  // to do revalidte indivudal sub section pages
+
+  return NextResponse.json({ received: true, now: Date.now(), data: data });
+}
