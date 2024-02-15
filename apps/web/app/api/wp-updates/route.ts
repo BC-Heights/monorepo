5278c51a1cdcb3bd18eee4fcb5197689848a3d44
shipmaal <100368040+shipmaal@users.noreply.github.com>
2024-02-15 12:32:56 -0500

    Refactor wp-updates route and add logging

--- apps/web/app/api/wp-updates/route.ts
+++ apps/web/app/api/wp-updates/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
-import { PostData } from './PostData';
 import { Logger } from 'next-axiom';
 
 export async function POST(req: NextRequest) {
@@ -10,28 +9,14 @@ export async function POST(req: NextRequest) {
     return NextResponse.error();
   }
 
-  log.debug('POST', data);
+  log.info('POST', data);
   console.log('POST', data);
+  if (data.taxonomies.category) {
+    const cats = Object.keys(data.taxonomies.category).map(
+      (key) => data.taxonomies.category[key].name
+    );
+    cats.forEach((cat) => revalidateTag(cat));
+  }
   await log.flush();
   return NextResponse.json({ received: true, now: Date.now(), data });
 }
-// }
-// const postData = data as PostData;
-// const apiKey = req.headers.get('x-api-key');
-// if (apiKey !== process.env.WP_WEBHOOK) {
-//   return NextResponse.error();
-// }
-
-// const cats = Object.keys(postData.taxonomies.category).map(
-//   (key) => data.taxonomies.category[key].name
-// );
-// log.debug('POST', {
-//   ...postData,
-//   post: { ...postData.post, post_content: undefined },
-//   post_before: undefined,
-// });
-
-// cats.forEach((cat) => revalidateTag(cat));
-// await log.flush();
-// return NextResponse.json({ received: true, now: Date.now(), data: postData });
-// }
