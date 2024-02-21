9b64187a8b8fde35cb14ddd251522a41913c588b
shipmaal <100368040+shipmaal@users.noreply.github.com>
2024-02-21 13:37:22 -0500

    Added basic carousel functionality to arts page, will need to add to mag

--- apps/web/.next/server/server-reference-manifest.js
+++ apps/web/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sB/yoJTEY6M+n5dEqwemioPJvyy35CO1czmSXLQF7i8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oBVuK97vP2xkMtfn6HHGhmv7WP625RbuAcApJlX1JTs=\"\n}"
