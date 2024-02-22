b9d0ff668a48257381ec466524555b5c81fa14b5
Harrison Cutler <cutler.cf@bc.edu>
2024-02-22 15:59:52 -0500

    test commit

--- apps/web/app/sports/page.tsx
+++ apps/web/app/sports/page.tsx
@@ -1,9 +1,10 @@
-import { SubTopicCard } from "@the-heights/components"
+import { SubTopicCard } from '@the-heights/components';
 
 export default function Page() {
-    return (
-        <div>
-            <SubTopicCard slug="sports" />
-        </div>
-    )
+  return (
+    <div>
+      <SubTopicCard slug="sports" />
+    </div>
+  );
 }
+// Commit Test Comment
