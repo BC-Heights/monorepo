7cd960b49516d7e9343d3a6688e6b67d1f90e7ad
shipmaal <100368040+shipmaal@users.noreply.github.com>
2024-02-25 11:55:07 -0500

    Update Prettier configuration and add Tailwind CSS plugin*** ***Update app-paths-manifest.json*** ***Update Prettier version and add prettier-plugin-tailwindcss*** ***Update styles in arts/page.tsx*** ***Update styles in topic-card.tsx*** ***Update number of posts to fetch in arts-card.tsx

--- libs/components/src/lib/arts-card.tsx
+++ libs/components/src/lib/arts-card.tsx
@@ -8,19 +8,19 @@ export interface ArtsCardProps {
 }
 
 export default async function ArtsCard(props: ArtsCardProps) {
-  const numberOfPostsToFetch = 6;
+  const numberOfPostsToFetch = 4;
 
   const { posts } = await GetPostsByCat(
     { first: numberOfPostsToFetch, categoryName: props.category },
-    [props.category]
+    [props.category],
   );
 
   return (
     <div className="h-auto">
       <div className="flex flex-row items-start gap-4">
-        <h1 className="text-2xl text-[#98002E] font-bold">{props.category}</h1>
+        <h1 className="text-2xl font-bold text-[#98002E]">{props.category}</h1>
       </div>
-      <div className="grid grid-cols-3 gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
         {posts?.nodes.map((post, index) => (
           <div key={index} className="relative">
             <Link href={`/${formatHrefDate(post.date!)}/${post.slug!}`}>
@@ -32,8 +32,13 @@ export default async function ArtsCard(props: ArtsCardProps) {
                   }
                   alt={post.featuredImage?.node.caption || 'No Alt'}
                 />
-                <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-[#98002E] bg-opacity-0 group-hover:bg-opacity-40">
-                  <p className="text-white text-lg font-bold text-center">
+                <div
+                  className="absolute inset-0 flex flex-col items-center justify-center 
+                           bg-[#98002E] bg-opacity-40 opacity-100 transition-opacity duration-300 
+                           sm:bg-opacity-0 sm:opacity-0
+                             group-hover:bg-opacity-40 group-hover:opacity-100"
+                >
+                  <p className="text-center text-lg font-bold text-white">
                     {post.title}
                   </p>
                   <AuthorName {...post} />
