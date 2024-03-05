57505d3c1ac2479dd547a91c54d2acb7683c3dec
shipmaal <100368040+shipmaal@users.noreply.github.com>
2024-03-05 15:40:46 -0600

    Making changes to the parser, also made changes to the webhook api for debugging

--- apps/web/app/api/wp-updates/route.ts
+++ apps/web/app/api/wp-updates/route.ts
@@ -1,33 +1,42 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { revalidatePath, revalidateTag } from 'next/cache';
-import { Logger } from 'next-axiom';
+import { withAxiom, AxiomRequest } from 'next-axiom';
 
-export async function POST(req: NextRequest) {
-  const log = new Logger();
+export const POST = withAxiom(async (req: AxiomRequest) => {
   const data = await req.json();
   if (!data) {
     return NextResponse.error();
   }
-  console.log('Received data:', data);
+
+  const log = req.log.with({ scope: 'wp-webhooks' });
+  log.debug('Received data:', data);
 
   if (data.taxonomies.category) {
     const cats = Object.keys(data.taxonomies.category).map(
-      (key) => data.taxonomies.category[key].name
+      (key) => data.taxonomies.category[key].name,
     );
     cats.forEach((cat) => revalidateTag(cat));
-    console.log('Revalidated categories:', cats);
+    log.debug('Revalidated categories:', cats);
   }
 
   if (data.post_permalink) {
     const post_permalink: string = data.post_permalink;
-    console.log('Revalidated post:', post_permalink);
-    if (!post_permalink.includes('?')){
-      revalidatePath(post_permalink.replace('www.bcheights.com', 'monorepo-chi-seven.vercel.app'));
-      console.log('Revalidated post:', post_permalink.replace('www.bcheights.com', 'monorepo-chi-seven.vercel.app'));
-      revalidatePath('/')
+    if (!post_permalink.includes('?')) {
+      revalidatePath(
+        post_permalink.replace(
+          'www.bcheights.com',
+          'monorepo-chi-seven.vercel.app',
+        ),
+      );
+      log.debug('Revalidated post:', {
+        url: post_permalink.replace(
+          'www.bcheights.com',
+          'monorepo-chi-seven.vercel.app',
+        ),
+      });
+      revalidatePath('/');
     }
   }
 
-  await log.flush();
-  return NextResponse.json({ received: true, now: Date.now(), data });
-}
+  return NextResponse;
+});
